1c06a14067e4b2a303bf9a2f845061b244249432
ImJustRob <robertalothian@hotmail.com>
2024-09-02 20:35:13 -0400

    add model and initial data

---
+++ db/migrate/20240903003059_create_homes.rb
@@ -0,0 +1,16 @@
+class CreateHomes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :homes do |t|
+      t.string :description
+      t.string :year_built
+      t.string :bedrooms
+      t.string :bathrooms
+      t.string :floors
+      t.string :availability
+      t.string :address
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end

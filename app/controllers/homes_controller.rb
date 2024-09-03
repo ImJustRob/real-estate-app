15e9c4aa6ab2f51fe0581f5cb9a37ff12c74b22d
ImJustRob <robertalothian@hotmail.com>
2024-09-02 22:21:39 -0400

    add create action

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,4 +8,16 @@ class HomesController < ApplicationController
         @home = Home.find_by(id: params[:id])
         render :show
     end
+    def create
+        @photo = Photo.create(
+            description: params[:description],
+            year_built: params[:year_built],
+            bedrooms: params[:bedrooms],
+            bathrooms: params[:bathrooms],
+            floors: params[:floors],
+            availability: params[:availability],
+            address: params[:address],
+            price: params[:price],
+        )
+        render :show
 end

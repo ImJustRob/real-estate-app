c60f58bf4debbf83de6fe855f250493a578d37c8
ImJustRob <robertalothian@hotmail.com>
2024-09-02 21:33:51 -0400

    add index action

--- db/migrate/20240903003059_create_homes.rb
+++ db/migrate/20240903003059_create_homes.rb
@@ -3,6 +3,7 @@ class CreateHomes < ActiveRecord::Migration[7.1]
     create_table :homes do |t|
       t.string :description
       t.string :year_built
+      t.string :square_feet
       t.string :bedrooms
       t.string :bathrooms
       t.string :floors
